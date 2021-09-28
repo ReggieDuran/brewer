ed3f87f838bc4b89826683d3d4d3da40028956e6
Reggie <reggieduran15@gmail.com>
2021-09-28 16:12:53 +0800

    update host

--- wp-config.php
+++ wp-config.php
@@ -29,7 +29,7 @@ define( 'DB_USER', 'sql6440664' );
 define( 'DB_PASSWORD', '82zUfURhxQ' );
 
 /** MySQL hostname */
-define( 'DB_HOST', 'localhost' );
+define( 'DB_HOST', 'sql6.freesqldatabase.com' );
 
 /** Database Charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8mb4' );
